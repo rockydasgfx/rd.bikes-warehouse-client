925e0ac4f6c070fa4541c89d13adb961263ffc9a
unknown <rockydasgfx@gmail.com>
2022-07-18 23:28:16 +0600

    banner complete

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner__container">
+      <div className="text-white w-50">
+        <h1 className="text-center">WELCOME TO RD.BIKES WAREHOUSE</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
